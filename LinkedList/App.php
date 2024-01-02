8eede8d94641d04e09c12ef985dbb22718cf07da
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2024-01-02 21:34:04 +0700

    bikin append dan prepend single linkedlist

--- LinkedList/App.php
+++ LinkedList/App.php
@@ -1,1 +1,14 @@
 <?php
+
+require_once "SingleLinkedList.php";
+
+$person1 = new SingleLinkedList();
+$person1->prepend("syahira");
+$person1->prepend("isnaeni");
+$person1->prepend("dewi");
+//var_dump($person1);
+
+$person2 = new SingleLinkedList();
+$person2->append("Syahira");
+$person2->append("Isnaeni");
+var_dump($person2);
--- LinkedList/SingleLinkedList.php
+++ LinkedList/SingleLinkedList.php
@@ -1,1 +1,67 @@
 <?php
+
+class NodeSingleLinkedList{
+    private $node;
+    private $next;
+    public function __construct($node)
+    {
+        $this->node = $node;
+        $this->next = null;
+    }
+
+    public function getNode()
+    {
+        return $this->node;
+    }
+
+    public function setNode($node)
+    {
+        $this->node = $node;
+    }
+
+    public function getNext()
+    {
+        return $this->next;
+    }
+
+    public function setNext($next)
+    {
+        $this->next = $next;
+    }
+}
+
+class SingleLinkedList {
+    private $head;
+
+    public function __construct()
+    {
+        $this->head = null;
+    }
+
+    public function prepend($value){
+        $newNode = new NodeSingleLinkedList($value);
+
+        if($this->head != null){
+            $newNode->setNext($this->head);
+        }
+
+        $this->head = $newNode;
+    }
+
+    public function append($value){
+        $newNode = new NodeSingleLinkedList($value);
+
+        if($this->head == null){
+            $this->head = $newNode;
+        }else{
+            $current = $this->head;
+
+            while($current->getNext() != null){
+                $current = $current->getNext();
+            }
+
+            $current->setNext($newNode);
+        }
+    }
+
+}
