14d1912bb9d1fc19df49de8a4f1c68a556964be9
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2024-01-06 10:54:23 +0700

    bikin append dan find di double linked list

--- LinkedList/DoubleLinkedList.php
+++ LinkedList/DoubleLinkedList.php
@@ -63,12 +63,57 @@ class DoubleLinkedList{
         $newNode = new NodeDoubleLinkedList($value);
         if ($this->head == null){
             $this->head = $newNode;
+            $this->tail = $newNode;
         }else{
             $this->head->setPrev($newNode);
             $temp = $this->head;
             $this->head = $newNode;
             $this->head->setNext($temp);
         }
+    }
+
+    public function append($value){
+        $newNode = new NodeDoubleLinkedList($value);
+        if($this->tail == null){
+            $this->head = $newNode;
+            $this->tail = $newNode;
+        }else{
+            $this->tail->setNext($newNode);
+            $temp = $this->tail;
+            $this->tail = $newNode;
+            $this->tail->setPrev($temp);
+        }
+    }
+
+    public function find($value): bool{
+        if($this->head != null){
+            if($this->head->getNode() == $value){
+                return true;
+            }
+
+            $current = $this->head;
+            while ($current->getNext() != null){
+                if($current->getNode() == $value){
+                    return true;
+                }
+                $current = $current->getNext();
+            }
+
+            if($current->getNode() == $value){
+                return true;
+            }
+
+            return false;
+        }
+        return false;
+    }
+
+    public function delete($value){
+
+    }
+
+    public function update($value, $newValue){
+
     }
 
 }
--- LinkedList/TestDoubleLinkedList.php
+++ LinkedList/TestDoubleLinkedList.php
@@ -2,10 +2,30 @@
 
 require_once "DoubleLinkedList.php";
 
+//TEST PREPEND DOUBLE LINKED LIST
 $person1 = new DoubleLinkedList();
-var_dump($person1);
+//var_dump($person1);
 $person1->prepend("Syahira");
-var_dump($person1);
+//var_dump($person1);
 $person1->prepend("Isnaeni");
 $person1->prepend("Dewi");
-var_dump($person1);
+//var_dump($person1);
+
+//TEST APPEND DOUBLE LINKED LIST
+$person2 = new DoubleLinkedList();
+//var_dump($person2);
+$person2->append("Syahira");
+//var_dump($person2);
+$person2->append("Isnaeni");
+$person2->append("Dewi");
+//var_dump($person2);
+
+//TEST FIND DOUBLE LINKED LIST
+$person3 = new DoubleLinkedList();
+var_dump($person3->find("Syahira"));
+$person3->append("Syahira");
+var_dump($person3->find("Syahira"));
+$person3->append("Isnaeni");
+$person3->append("Dewi");
+var_dump($person3->find("Dewi"));
+var_dump($person3->find("Joko"));
